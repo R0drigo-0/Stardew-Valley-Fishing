f988384611ff5465a74dbc6f25504e1600abfebb
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2025-03-26 10:24:12 +0100

    App

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import { fileURLToPath } from "url";
+import express from "express";
 import cors from "cors";
 import path from "path";
-import express from "express";
 
-const Game = require("./game.js");
+import Game from "./game.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,15 +20,46 @@ const game = Game();
 
 // Implement your endpoints here...
 app.get("/cast_line", (req, res) => {
-  playerState = game.castLine();
-  return res.json({ playerState });
+  const playerState = game.castLine();
+  if (result !== "line_cast") {
+    return res.status(400).json({ errorCode: result });
+  }
+  return res.status(200).json({ playerState: result });
 });
 
-app.get("/wait_for_bite", (req, res) => {});
+app.get("/wait_for_bite", (req, res) => {
+  game
+    .waitForBite()
+    .then(() => {
+      res.status(200).json({ success: true });
+    })
+    .catch((errorCode) => {
+      res.status(400).json({ errorCode });
+    });
+});
 
-app.get("/reel_in", (req, res) => {});
+app.get("/reel_in", (req, res) => {
+  const result = game.reelIn();
+  if (result.errorCode) {
+    return res.status(400).json(result);
+  }
+  return res.status(200).json(result);
+});
 
-app.get("/get_mini_game_info", (req, res) => {});
+app.get("/get_mini_game_info", (req, res) => {
+  const gameInfo = game.getCatchingMiniGameInfo();
+  return res.status(200).json(gameInfo);
+});
+
+app.get("/move_catch_bar_up", (req, res) => {
+  game.updateCatchBarDirection("up");
+  res.status(200).json({ success: true });
+});
+
+app.get("/stop_moving_catch_bar_up", (req, res) => {
+  game.updateCatchBarDirection("none");
+  res.status(200).json({ success: true });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
